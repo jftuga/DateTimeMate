bfa06ed33301521ad401b469e8d02b463831c891
John Taylor <jftuga@users.noreply.github.com>
2026-07-04 12:05:15 -0400

    version bump to 1.4.1

--- DateTimeMate.go
+++ DateTimeMate.go
@@ -21,7 +21,7 @@ var ReadmeMd string
 
 const (
 	ModName    string = "DateTimeMate"
-	ModVersion string = "1.4.0"
+	ModVersion string = "1.4.1"
 	ModUrl     string = "https://github.com/jftuga/DateTimeMate"
 )
 
